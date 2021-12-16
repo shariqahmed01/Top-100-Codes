7bf794a09910a49cc857e756842370fec76058fd
Shariq Ahmed <67710319+stack-overload@users.noreply.github.com>
2021-12-16 22:23:50 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,19 @@
+# take first number as input from user
+first = int(input("Enter first number:"))
+
+#take second number as input from user
+second = int(input("Enter second number:"))
+
+#use if else to find answer
+
+#check if first number is greater
+if first > second:
+    print(first)
+
+#check if second number is greater
+elif second>first:
+    print(second)
+
+#Number is equal
+else:
+     print("equal") 
